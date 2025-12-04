cbf5e83d4b6560986d64ba1e613826d54edc4158
mohannad <119969501+mov44@users.noreply.github.com>
2025-12-04 13:48:43 +0200

    Add shadcn initialization and updated side-bar and table

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,11 +1,13 @@
 import TopBar from "./components/TopBar";
 import SideBar from "./components/SideBar";
+import { TableDemo } from "./components/Users";
 
 export default function Dashboard() {
   return (
-    <div>
+    <div className="relative ">
       <TopBar />
       <SideBar />
+      <TableDemo />
     </div>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -35,8 +35,8 @@
 import {Link} from "react-router-dom"
 export default function SideBar() {
   return (
-    <nav className="hidden md:block bg-white shadow-md border-r border-gray-200   h-screen fixed  left-0 min-w-[150px] py-6 px-4 ">
-      <ul>
+    <nav className="hidden md:block bg-white shadow-md border-r border-gray-200   h-screen fixed  left-0 w-[150px] py-6 px-4 ">
+      {/* <ul>
         <li>
           <a
             href="javascript:void(0)"
@@ -45,13 +45,18 @@ export default function SideBar() {
             Dashboard
           </a>
         </li>
-      </ul>
+      </ul> */}
 
-      <div className="mt-4">
-        <ul className="mt-2 space-y-1">
+      <div className="mt-4 -ml-10">
+        <ul className="mt-2 space-y-4 ">
+          <li>
+            <a className="text-slate-700 font-medium text-[15px] block hover:text-slate-900 hover:bg-gray-100 rounded transition-all">
+              Dashboard
+            </a>
+          </li>
           <li>
             <Link to="/dashboard/users" className="nav-link text-black">
-               Home {" "}
+              Home{" "}
             </Link>
           </li>
         </ul>
